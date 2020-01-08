efbda2727c4d5832f1ee8050e27ddfc0def0b2e8
Christian Montero Martínez <christianmontero08@gmail.com>
2020-01-07 22:48:48 -0800

    Add form for the messages and show incomming messages in browser

--- public/index.js
+++ public/index.js
@@ -1,14 +1,20 @@
 var socket = io();
+
+jQuery('#message-form').on('submit', function(e){
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    })
+})
+
 socket.on('connect', function(){
     console.log("Connected to the server");
 
-    // socket.emit('createMessage', {
-    //     from: 'Christian',
-    //     text: 'Hey, this is Chris'
-    // });
-
     socket.on('newMessage', (message) => {
-        console.log("The new Message is: ", message);
+        var li = jQuery('<li></li>');
+        li.text(`${message.from}: ${message.text}`);
+        jQuery('#messages').append(li);
     });
 });
 
